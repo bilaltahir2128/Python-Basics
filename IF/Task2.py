be7c626499e1c104441f244ca737c3366f7e4aa7
Muhammad Bilal Tahir <bilaltahir2128@gmail.com>
2025-05-19 01:05:53 +0500

    Delete IF/Task2.py

--- IF/Task2.py
+++
@@ -1,9 +0,0 @@
-sugar=input("Enter your fasting sugar level:")
-sugar=float(sugar)
-
-if sugar <80:
-    print("Your sugar level is low.")
-elif sugar>100:
-    print("Your sugar level is high.")
-else:
-    print("Your sugar level is normal.")
