705ce50956a30b4335820f86fc1739df47b6eb01
Muhammad Bilal Tahir <bilaltahir2128@gmail.com>
2025-05-19 01:02:05 +0500

    Add files via upload

---
+++ Variables/Task2.py
@@ -0,0 +1,7 @@
+first_name='Muhammad '
+middle_name='Bilal '
+last_name='Tahir'
+Full_name=first_name+middle_name+last_name
+print(Full_name)
+# OR
+print("My full name is : "+first_name+" "+middle_name+" "+last_name)
