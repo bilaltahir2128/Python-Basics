58931c01599395074bfdcc508c88538d56d2c70d
Muhammad Bilal Tahir <bilaltahir2128@gmail.com>
2025-05-19 00:27:15 +0500

    Delete Variables/Variables directory

--- Variables/Variables/Task1.py
+++
@@ -1,5 +0,0 @@
-birth_date=2005
-current_year=2025
-Age=current_year-birth_date
-print(Age)
-
--- Variables/Variables/Task2.py
+++
@@ -1,7 +0,0 @@
-first_name='Muhammad '
-middle_name='Bilal '
-last_name='Tahir'
-Full_name=first_name+middle_name+last_name
-print(Full_name)
-# OR
-print("My full name is : "+first_name+" "+middle_name+" "+last_name)
